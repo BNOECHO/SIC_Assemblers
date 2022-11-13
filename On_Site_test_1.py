5bc606813775f8de24bcdd5ebf84ecfc58ea0ff3
Violet's Foxes <111033412+KoukeNeko@users.noreply.github.com>
2022-11-13 16:52:52 +0800

    clean

--- On_Site_test_1.py
+++ On_Site_test_1.py
@@ -4,32 +4,7 @@ start_Address = int()
 SYMTAB = dict() # string, int
 
 Opcode = { #SIC模式下理論上opcode不會被動到 故直接存字串
-    "Add": "18",
-    "AND": "40",
-    "COMP": "28",
-    "DIV": "24",
-    "J": "3C",
-    "JEQ": "30",
-    "JGT": "34",
-    "JLT": "38",
-    "JSUB": "48",
-    "LDA": "00",
-    "LDCH": "50",
-    "LDL": "08",
-    "LDX": "04",
-    "MUL": "20",
-    "OR": "44",
-    "RD": "D8",
-    "RSUB": "4C",
-    "STA": "0C",
-    "STCH": "54",
-    "STL": "14",
-    "STSW": "E8",
-    "STX": "10",
-    "SUB": "1C",
-    "TD": "E0",
-    "TIX": "2C",
-    "WD": "DC"
+    "Add": "18","AND": "40","COMP": "28","DIV": "24","J": "3C","JEQ": "30","JGT": "34","JLT": "38","JSUB": "48","LDA": "00","LDCH": "50","LDL": "08","LDX": "04","MUL": "20","OR": "44","RD": "D8","RSUB": "4C","STA": "0C","STCH": "54","STL": "14","STSW": "E8","STX": "10","SUB": "1C","TD": "E0","TIX": "2C","WD": "DC"
 }
 
 class SIC_Line: #指令類別
